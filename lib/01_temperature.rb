2615c267831fabd7ab115fb8d7017c2bb61cd574
gustavokr87 <gustavokr@gmail.com>
2019-10-07 18:39:10 +0200

    firstcommit

---
+++ lib/01_temperature.rb
@@ -0,0 +1,6 @@
+def ftoc(num)
+   ((num - 32) * 5.0 / 9.0)
+ end
+ def ctof(num)
+   ((num * 9.0 / 5.0) + 32.0)
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,16 @@
+
+def add(num)
+  0 + 0 = 0
+end
+.
+def add2(num)
+  2 + 2 = 4
+end
+
+def addspositive
+  2 + 6 = 8
+end
+
+def method_name
+
+end
